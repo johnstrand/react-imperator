2274ff7908c811e06251fa1c31d03d26e178dcb1
John Strand <john.strand@dekiru.se>
2018-09-28 17:58:05 +0200

    Added a few comments

--- src/ReactImperator.tsx
+++ src/ReactImperator.tsx
@@ -4,22 +4,29 @@ import { ContextCallback, CallbackRegistration, Callback, Func, IndexedObject, V
 import { generateName, distinct } from "./Utils";
 
 export const { update, connect } = (() => {
+    // Tracks all of the context callbacks, with the structure
+    // callbacks.context.consumer = callback
     const callbacks: ContextCallback = {};
 
+    // Tracks the last value specified for each context
     const contextState: { [key: string]: any } = {};
 
+    // Registers a callback for a specific combination of context and consumer
     const registerCallback: CallbackRegistration = (
         context: string,
         consumer: string,
         callback: Callback
     ): void => {
+        // If this specific context has never been seen before, populate the root object
         if (!callbacks[context]) {
             callbacks[context] = {};
         }
+        // Assign the callback to the context/consumer combo
         callbacks[context][consumer] = callback;
     };
 
     const unregister: Func<string, void> = (consumer: string) => {
+        // Remove the specified consumer entry for each context
         Object.keys(callbacks).forEach(context => delete callbacks[context][consumer]);
     };
 
@@ -61,7 +68,10 @@ export const { update, connect } = (() => {
                             const newState: IndexedObject = { [context]: value };
                             this.setState((state: S) => ({ ...(state as {}), ...newState }));
                         });
-                        if (propContexts.indexOf(context) > -1) {
+
+                        // If the current context comes from a component prop AND
+                        // the context doesn't already have a value, take the property value
+                        if (propContexts.indexOf(context) > -1 && !contextState[context]) {
                             const value: IndexedObject = (props as IndexedObject)[context];
                             contextState[context] = value;
                         }
