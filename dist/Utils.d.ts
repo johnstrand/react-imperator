aa7fc8c7a390d2d0c8772c3de084c7ad5573a2e1
John Strand <john.strand@dekiru.se>
2018-09-29 21:07:47 +0200

    Updated build artefacts

--- dist/Utils.d.ts
+++ dist/Utils.d.ts
@@ -1,3 +1,4 @@
-import { Action, Func } from "./Types";
+import { Action, Func, Func2 } from "./Types";
 export declare const generateName: Action<string>;
 export declare const distinct: Func<string[], string[]>;
+export declare const exclude: Func2<string[], string[], string[]>;
