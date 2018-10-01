95b2af959145720b9e3f7b79c27e88be283e557f
John Strand <john.strand@dekiru.se>
2018-10-01 18:36:28 +0200

    Updated build output

--- dist/ReactImperator.d.ts
+++ dist/ReactImperator.d.ts
@@ -1,4 +1,2 @@
 import * as React from "react";
-export declare module ReactImperator {
-    const update: <T>(context: string, producer: (state: T) => T) => void, connect: <S>(Component: React.ComponentType<S>, contexts?: string[], excludedContexts?: string[]) => React.ComponentType<S>;
-}
+export declare const connect: <S>(Component: React.ComponentType<S>, contexts?: string[], excludedContexts?: string[]) => React.ComponentType<S>, update: <T>(context: string, producer: (state: T) => T) => void;
