11d1540307515c0f255356f4ae7e3df49fdb4060
John Strand <john.strand@dekiru.se>
2018-10-01 18:36:08 +0200

    Restructured export a bit

--- src/ReactImperator.tsx
+++ src/ReactImperator.tsx
@@ -7,111 +7,105 @@ type Imperator = {
     update: <T>(context: string, producer: (state: T) => T) => void,
     connect: <S>(Component: React.ComponentType<S>, contexts?: string[], excludedContexts?: string[]) => React.ComponentType<S>
 }
-export module ReactImperator {
-    export const { update, connect }: Imperator = (() => {
-        // Tracks all of the context callbacks, with the structure
-        // callbacks.context.consumer = callback
-        const callbacks: ContextCallback = {};
+ const reactImperator: Imperator = (() => {
+    // Tracks all of the context callbacks, with the structure
+    // callbacks.context.consumer = callback
+    const callbacks: ContextCallback = {};
 
-        // Tracks the last value specified for each context
-        const contextState: { [key: string]: any } = {};
+    // Tracks the last value specified for each context
+    const contextState: { [key: string]: any } = {};
 
-        // Registers a callback for a specific combination of context and consumer
-        const registerCallback: CallbackRegistration = (
-            context: string,
-            consumer: string,
-            callback: Callback
-        ): void => {
-            // If this specific context has never been seen before, populate the root object
+    // Registers a callback for a specific combination of context and consumer
+    const registerCallback: CallbackRegistration = (
+        context: string,
+        consumer: string,
+        callback: Callback
+    ): void => {
+        // If this specific context has never been seen before, populate the root object
+        if (!callbacks[context]) {
+            callbacks[context] = {};
+        }
+        // Assign the callback to the context/consumer combo
+        callbacks[context][consumer] = callback;
+    };
+
+    const unregister: Func<string, void> = (consumer: string) => {
+        // Remove the specified consumer entry for each context
+        Object.keys(callbacks).forEach(context => delete callbacks[context][consumer]);
+    };
+
+    const updateContext: VoidAction2<string, any> = (context: string, state: any) => {
+        Object.keys(callbacks[context]).forEach(subscriber => {
+            const callback: Callback = callbacks[context][subscriber];
+            if (!callback) {
+                return;
+            }
+            callback(state);
+        });
+    };
+
+    return {
+        update: function <T>(context: string, producer: (state: T) => T): void {
             if (!callbacks[context]) {
-                callbacks[context] = {};
+                return;
             }
-            // Assign the callback to the context/consumer combo
-            callbacks[context][consumer] = callback;
-        };
 
-        const unregister: Func<string, void> = (consumer: string) => {
-            // Remove the specified consumer entry for each context
-            Object.keys(callbacks).forEach(context => delete callbacks[context][consumer]);
-        };
+            const state: any = producer(contextState[context]);
+            contextState[context] = state;
+            updateContext(context, state);
+        },
+        connect: function <S>(
+            Component: React.ComponentType<S>,
+            contexts?: string[],
+            excludedContexts?: string[]
+        ): React.ComponentType<S> {
 
-        const updateContext: VoidAction2<string, any> = (context: string, state: any) => {
-            Object.keys(callbacks[context]).forEach(subscriber => {
-                const callback: Callback = callbacks[context][subscriber];
-                if (!callback) {
-                    return;
-                }
-                callback(state);
-            });
-        };
+            const forEachProp = (props: S, callback: (prop: string) => void) => {
+                const propContexts: string[] = Object.keys(props);
+                const suppliedContexts: string[] = contexts || [];
+                exclude(distinct(propContexts.concat(suppliedContexts)), (excludedContexts || [])).forEach(callback);
+            }
 
-        return {
-            update: function <T>(context: string, producer: (state: T) => T): void {
-                if (!callbacks[context]) {
-                    return;
-                }
+            return class extends React.Component<S, S> {
+                private name: string;
+                constructor(props: S) {
+                    super(props);
+                    this.name = generateName();
 
-                const state: any = producer(contextState[context]);
-                contextState[context] = state;
-                updateContext(context, state);
-            },
-            connect: function <S>(
-                Component: React.ComponentType<S>,
-                contexts?: string[],
-                excludedContexts?: string[]
-            ): React.ComponentType<S> {
-
-                const forEachProp = (props: S, callback: (prop: string) => void) => {
-                    const propContexts: string[] = Object.keys(props);
-                    const suppliedContexts: string[] = contexts || [];
-                    exclude(distinct(propContexts.concat(suppliedContexts)), (excludedContexts || [])).forEach(callback);
-                }
-
-                return class extends React.Component<S, S> {
-                    private name: string;
-                    constructor(props: S) {
-                        super(props);
-                        this.name = generateName();
-
-                        forEachProp(props, context => {
-                            registerCallback(context, this.name, value => {
-                                if (!context) {
-                                    return;
-                                }
-                                const newState: IndexedObject = { [context]: value };
-                                this.setState((state: S) => ({ ...(state as {}), ...newState }));
-                            });
-
-                            // If the current context comes from a component prop AND
-                            // the context doesn't already have a value, take the property value
-                            if ((props as IndexedObject)[context] && !contextState[context]) {
-                                const value: IndexedObject = (props as IndexedObject)[context];
-                                contextState[context] = value;
+                    forEachProp(props, context => {
+                        registerCallback(context, this.name, value => {
+                            if (!context) {
+                                return;
                             }
+                            const newState: IndexedObject = { [context]: value };
+                            this.setState((state: S) => ({ ...(state as {}), ...newState }));
                         });
 
-                        this.state = contextState as S;
-                    }
+                        // If the current context comes from a component prop AND
+                        // the context doesn't already have a value, take the property value
+                        if ((props as IndexedObject)[context] && !contextState[context]) {
+                            const value: IndexedObject = (props as IndexedObject)[context];
+                            contextState[context] = value;
+                        }
+                    });
 
-                    render(): JSX.Element {
-                        return <Component {...this.state} />;
-                    }
+                    this.state = contextState as S;
+                }
 
-                    componentWillMount(): void {
-                        forEachProp(this.props, context => {
-                            update<any>(context, value => value);
-                        });
-                    }
+                render(): JSX.Element {
+                    return <Component {...this.state} />;
+                }
 
-                    componentWillReceiveProps(props: S): void {
-                        this.setState(props);
-                    }
+                componentWillReceiveProps(props: S): void {
+                    this.setState(props);
+                }
 
-                    componentWillUnmount(): void {
-                        unregister(this.name);
-                    }
-                };
-            }
-        };
-    })();
-}
+                componentWillUnmount(): void {
+                    unregister(this.name);
+                }
+            };
+        }
+    };
+})();
+
+export const { connect, update } = reactImperator;
