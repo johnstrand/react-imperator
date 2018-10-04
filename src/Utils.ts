d5f59ac9581052aebfb813807c92415477267896
John Strand <john.strand@dekiru.se>
2018-10-04 19:26:48 +0200

    Formatting adjustment

--- src/Utils.ts
+++ src/Utils.ts
@@ -5,7 +5,7 @@ export const generateName: Action<string> = (): string =>
         .toString(36)
         .substring(7);
 
-export const distinct : Func<string[], string[]> = (arr: string[]) => {
+export const distinct: Func<string[], string[]> = (arr: string[]) => {
     const lookup: UniqueIndexer =
         arr.reduce((acc: UniqueIndexer, cur: string) => {
             acc[cur] = true;
@@ -15,12 +15,12 @@ export const distinct : Func<string[], string[]> = (arr: string[]) => {
     return Object.keys(lookup);
 };
 
-export const exclude : Func2<string[], string[], string[]> = (sourceArray: string[], excludeArray: string[]) => {
+export const exclude: Func2<string[], string[], string[]> = (sourceArray: string[], excludeArray: string[]) => {
     const excludeLookup: UniqueIndexer =
         excludeArray.reduce((acc: UniqueIndexer, cur: string) => {
             acc[cur] = true;
             return acc;
         }, {});
-    
+
     return sourceArray.filter(item => !excludeLookup[item]);
 };
