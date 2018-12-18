9b4befceff02505cfe694a79cc43c788ccc7316c
John Strand <john.strand@dekiru.se>
2018-12-18 09:15:08 +0100

    Updated dist artefacts

--- dist/ReactImperator.d.ts
+++ dist/ReactImperator.d.ts
@@ -1,2 +1,2 @@
-import * as React from "react";
+import * as React from 'react';
 export declare const connect: <S>(Component: React.ComponentType<S>, contexts?: string[], excludedContexts?: string[]) => React.ComponentType<S>, update: <T>(context: string, producer: (state: T) => T) => void;
