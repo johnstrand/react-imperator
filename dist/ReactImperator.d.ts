78087923bd670da9f7e80f48ba50ae574dccd7dc
John Strand <john.strand@dekiru.se>
2019-01-31 10:27:24 +0100

    Updated build artefacts

--- dist/ReactImperator.d.ts
+++ dist/ReactImperator.d.ts
@@ -1,2 +1,8 @@
 import * as React from 'react';
-export declare const connect: <S>(Component: React.ComponentType<S>, contexts?: string[], excludedContexts?: string[]) => React.ComponentType<S>, update: <T>(context: string, producer: (state: T) => T) => void, subscribe: <T>(context: string, callback: (state: T) => void) => string, unsubscribe: (name: string) => void, get: <T>(context: string) => T;
+export declare const connect: <S>(Component: React.ComponentType<S>, contexts?: string[], excludedContexts?: string[]) => React.ComponentType<S>, update: <T>(context: string, producer: (state: T) => T) => void, subscribe: <T>(context: string, callback: (state: T) => void) => string, unsubscribe: (name: string) => void, get: <T>(context: string) => T, persist: {
+    (): void;
+    (context: string): void;
+}, restore: {
+    (): void;
+    (context: string): void;
+};
