757cb8a714284c1d431a7a01169da668d54943bf
John Strand <john.strand@dekiru.se>
2019-02-03 18:29:26 +0100

    Started work on 2.0

--- src/ReactImperator.tsx
+++ src/ReactImperator.tsx
@@ -1,163 +1,91 @@
 import * as React from "react";
 
-import {
-    Callback,
-    CallbackRegistration,
-    ContextCallback,
-    Func,
-    IndexedObject,
-    VoidAction2,
-} from "./Types";
+const generateName: () => string = (): string =>
+    Math.random()
+        .toString(36)
+        .substring(7);
 
-import { distinct, exclude, generateName } from "./Utils";
+export function createStore<StoreState>(initialState: StoreState) {
+    interface IDictionary<T> { [key: string]: T; }
+    type Notifier = () => void;
+    type StoreProp = keyof StoreState;
+    type Callback<T extends StoreProp> = (value: StoreState[T]) => void;
 
-interface Imperator {
-    update: <T>(context: string, producer: (state: T) => T) => void;
-    connect: <S>(
-        Component: React.ComponentType<S>,
-        contexts?: string[],
-        excludedContexts?: string[]
-    ) => React.ComponentType<S>;
-}
-
-const reactImperator: Imperator = (() => {
-    // Tracks all of the context callbacks, with the structure
-    // callbacks.context.consumer = callback
-    const callbacks: ContextCallback = {};
-
-    // Tracks the last value specified for each context
-    const contextState: { [key: string]: any } & object = {};
-
-    // Registers a callback for a specific combination of context and consumer
-    const registerCallback: CallbackRegistration = (
-        context: string,
-        consumer: string,
-        callback: Callback
-    ): void => {
-        // If this specific context has never been seen before, populate the root object
-        if (!callbacks[context]) {
-            callbacks[context] = {};
-        }
-
-        // Assign the callback to the context/consumer combo
-        callbacks[context][consumer] = callback;
-    };
-
-    const unregister: Func<string, void> = (consumer: string) => {
-        // Remove the specified consumer entry for each context
-        Object.keys(callbacks).forEach(
-            (context) => delete callbacks[context][consumer]
-        );
-    };
-
-    const updateContext: VoidAction2<string, any> = (
-        context: string,
-        state: any
-    ) => {
-        // No callbacks registered for this context, just do nothing
-        if (!callbacks[context]) {
-            return;
-        }
-        Object.keys(callbacks[context]).forEach((subscriber) => {
-            const callback: Callback = callbacks[context][subscriber];
-            if (!callback) {
-                return;
-            }
-            callback(state);
-        });
-    };
+    const store: StoreState = initialState;
+    const storeCallbacks: IDictionary<Notifier> & object = {};
+    const subscriberCallbacks: IDictionary<IDictionary<(value: any) => void>> &
+        object = {};
 
     return {
-        update<T>(context: string, producer: (state: T) => T): void {
-            const state: any = producer(contextState[context]);
-            contextState[context] = state;
-            updateContext(context, state);
+        subscribe<T extends StoreProp>(
+            context: T,
+            callback: Callback<T>
+        ): string {
+            const name: string = generateName();
+            if (!subscriberCallbacks.hasOwnProperty(context)) {
+                subscriberCallbacks[context as string] = {};
+            }
+            subscriberCallbacks[context as string][name] = callback;
+            return name;
         },
-        connect<S>(
-            Component: React.ComponentType<S>,
-            contexts?: string[],
-            excludedContexts?: string[]
-        ): React.ComponentType<S> {
-            const forEachProp = (
-                props: S,
-                callback: (prop: string) => void
-            ) => {
-                const propContexts: string[] = Object.keys(props);
-                const suppliedContexts: string[] = contexts || [];
-                exclude(
-                    distinct(propContexts.concat(suppliedContexts)),
-                    excludedContexts || []
-                ).forEach(callback);
-            };
+        unsubscribe(name: string): void {
+            Object.getOwnPropertyNames(subscriberCallbacks).forEach(
+                (context) => {
+                    delete subscriberCallbacks[context][name];
+                }
+            );
+        },
+        get<T extends StoreProp>(context: T): StoreState[T] {
+            return store[context];
+        },
+        update<T extends StoreProp>(
+            context: T,
+            reducer: (state: StoreState[T]) => StoreState[T]
+        ): void {
+            const newState: StoreState[T] = reducer(store[context]);
+            store[context] = newState;
 
-            return class extends React.Component<S, S> {
-                private name: string;
-                constructor(props: S) {
-                    super(props);
-                    if (!props) {
-                        props = {} as S;
-                    }
-                    this.name = generateName();
+            Object.getOwnPropertyNames(storeCallbacks).forEach((name) => {
+                storeCallbacks[name]();
+            });
 
-                    const initialState: IndexedObject = {};
-
-                    // Copy all existing props into the initial state
-                    Object.keys(props).forEach(
-                        (prop) =>
-                            (initialState[prop] = (props as IndexedObject)[
-                                prop
-                            ])
+            if (subscriberCallbacks.hasOwnProperty(context)) {
+                Object.getOwnPropertyNames(
+                    subscriberCallbacks[context as string]
+                ).forEach((subscriber) => {
+                    subscriberCallbacks[context as string][subscriber](
+                        newState
                     );
+                });
+            }
+        },
+        connect<LocalState>(
+            Component: React.ComponentType<LocalState>,
+            mapStoreToLocal: (state: StoreState) => LocalState
+        ): React.ComponentType<LocalState> {
+            return class extends React.Component<LocalState, LocalState> {
+                private name: string;
+                constructor(props: LocalState) {
+                    super(props);
+                    this.name = generateName();
+                    this.state = mapStoreToLocal({ ...props, ...store });
 
-                    forEachProp(props, (context) => {
-                        registerCallback(context, this.name, (value) => {
-                            if (!context) {
-                                return;
-                            }
-                            const newState: IndexedObject = {
-                                [context]: value,
-                            };
-                            this.setState((state: S) => ({
-                                ...(state as {}),
-                                ...newState,
-                            }));
-                        });
-
-                        // If the property exists in the global state, replace the value of the initial state
-                        if (contextState.hasOwnProperty(context)) {
-                            initialState[context] = contextState[context];
-                        }
-
-                        // If the current context comes from a component prop AND
-                        // the context doesn't already have a value, take the property value
-                        if (
-                            (props as IndexedObject)[context] &&
-                            !contextState.hasOwnProperty(context)
-                        ) {
-                            const value: any = (props as IndexedObject)[
-                                context
-                            ];
-                            contextState[context] = value;
-                        }
-                    });
-
-                    this.state = initialState as S;
+                    storeCallbacks[this.name] = () =>
+                        this.setState(mapStoreToLocal(store));
                 }
 
                 public render(): JSX.Element {
                     return <Component {...this.state} />;
                 }
 
-                public componentWillReceiveProps(props: S): void {
-                    this.setState(props);
-                }
-
                 public componentWillUnmount(): void {
-                    unregister(this.name);
+                    Object.getOwnPropertyNames(storeCallbacks).forEach(
+                        (context) => {
+                            delete storeCallbacks[context][name];
+                        }
+                    );
                 }
             };
         },
     };
-})();
-
-export const { connect, update } = reactImperator;
+}
